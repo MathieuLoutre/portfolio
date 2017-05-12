348915e2a4064f0e4557c3246d53208aae5b9afa
Mathieu Triay <mathieu.triay@gmail.com>
2017-05-12 15:58:55 +0100

    Use UnCSS to remove unused styles. Use advanced Typekit embed.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gutil = require('gulp-util')
 var stylus = require('gulp-stylus')
 var browserSync = require('browser-sync').create()
 var prefix = require('autoprefixer-stylus')
-var replace = require('gulp-replace')
 var bro = require('gulp-bro')
 var babelify = require('babelify')
+var uncss = require('gulp-uncss')
+var nano = require('gulp-cssnano')
 
 gulp.task('css', function () {
 	return gulp.src('./src/assets/styles/app.styl')
@@ -15,14 +16,25 @@ gulp.task('css', function () {
 			use: prefix(),
 			include: 'node_modules'
 		}))
-		.pipe(replace('@@root_path', ''))
+		.pipe(gulp.dest('./dist/assets'))
+		.pipe(browserSync.reload({ stream: true }))
+})
+
+gulp.task('css-prod', function () {
+	return gulp.src('./src/assets/styles/app.styl')
+		.pipe(plumber())
+		.pipe(stylus({
+			use: prefix(),
+			include: 'node_modules'
+		}))
+		.pipe(uncss({ html: ['./dist/index.html'], ignoreSheets: [/use.typekit/] }))
+		.pipe(nano())
 		.pipe(gulp.dest('./dist/assets'))
 		.pipe(browserSync.reload({ stream: true }))
 })
 
 gulp.task('copy-html', function () {
 	return gulp.src('./src/*.html')
-		.pipe(replace('@@root_path', ''))
 		.pipe(gulp.dest('./dist'))
 		.pipe(browserSync.reload({ stream: true }))
 })
@@ -61,5 +73,5 @@ gulp.task('serve', function () {
 	gulp.watch("./src/assets/js/*", ['js'])
 })
 
-gulp.task('default', ['copy-images', 'copy-html', 'css', 'js', 'serve'])
-gulp.task('build', ['copy-images', 'copy-html', 'css', 'js'])
+gulp.task('default', ['copy-images', 'copy-html', 'js', 'serve'])
+gulp.task('build', ['copy-images', 'copy-html', 'css-prod', 'js'])
